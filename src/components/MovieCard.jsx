c611c39407a24b1b3293640d8bef83ed77660889
Firat-Sahin <firatsahin4002qgmail.com>
2025-09-06 16:01:14 +0300

    project development and bug fix

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,24 +1,30 @@
+import "../css/MovieCard.css";
 
-function MovieCard({movie}) {
-     
-    function onFavoriteClick() {
-        alert("clicked")
-    }
+function MovieCard({ movie }) {
+  function onFavoriteClick() {
+    alert("clicked");
+  }
 
-    return <div className="movie-card"> 
-        <div className="movie-poster">
-            <img src= {movie.url} alt={movie.title}/>
-            <div className="movie-overlay">
-                <button className= "favorite-btn" onClick={onFavoriteClick} > 
-                    ❤️
-                </button>
-            </div>
+  const posterUrl = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    : "https://via.placeholder.com/500x750?text=No+Image"; // fallback resim
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={posterUrl} alt={movie.title} />
+        <div className="movie-overlay">
+          <button className="favorite-btn" onClick={onFavoriteClick}>
+            ❤️
+          </button>
         </div>
-        <div className="movie-info">
-            <h3>{movie.title}</h3>
-            <p>{movie.release_date}</p>
-        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
+      </div>
     </div>
+  );
 }
 
-export default MovieCard
+export default MovieCard;
